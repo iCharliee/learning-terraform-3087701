d3b99ead012afb8e6fc2bbe00d6515293f5b1e78
klechowicz <karol.lechowicz@dxc.com>
2024-09-15 15:42:14 +0200

    retrieve oldest versions

--- main.tf
+++ main.tf
@@ -15,8 +15,7 @@ data "aws_ami" "app_ami" {
 }
 
 module "blog_vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "3.18.1"
+  source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
   cidr = "10.0.0.0/16"
@@ -30,15 +29,17 @@ module "blog_vpc" {
   }
 }
 
+
 module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "8.0.0"
+  version = "6.5.2"
 
-  name                = "blog"
+  name = "blog"
+
   min_size            = 1
   max_size            = 2
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = [module.blog_alb.target_groups[0].arn]
+  target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
   instance_type       = var.instance_type
   image_id            = data.aws_ami.app_ami.id
@@ -46,13 +47,15 @@ module "blog_autoscaling" {
 
 module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
-  version = "9.11.0"
+  version = "~> 6.0"
 
-  name               = "blog-alb"
+  name = "blog-alb"
+
   load_balancer_type = "application"
-  vpc_id             = module.blog_vpc.vpc_id
-  subnets            = module.blog_vpc.public_subnets
-  security_groups    = [module.blog_sg.security_group_id]
+
+  vpc_id          = module.blog_vpc.vpc_id
+  subnets         = module.blog_vpc.public_subnets
+  security_groups = [module.blog_sg.security_group_id]
 
   target_groups = [
     {
@@ -63,7 +66,7 @@ module "blog_alb" {
     }
   ]
 
-  listeners = [
+  http_tcp_listeners = [
     {
       port               = 80
       protocol           = "HTTP"
@@ -78,7 +81,7 @@ module "blog_alb" {
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "5.2.0"
+  version = "4.13.0"
 
   vpc_id              = module.blog_vpc.vpc_id
   name                = "blog"
